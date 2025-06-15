32f5928003975133a06f98496d7d826da535de40
Promal <promal.barua.cse@gmail.com>
2025-06-15 16:08:27 +0600

    Initial Commit

---
+++ Teebay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Teebay
+//
+//  Created by Promal on 15/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Teebay/TeebayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeebayApp.swift
+//  Teebay
+//
+//  Created by Promal on 15/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TeebayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
